9d17ef7227d443dfcf35991081151c378fa7b639
James Hilliard <james.hilliard1@gmail.com>
2016-12-12 22:18:46 -0600

    Skip TestBlockValidity

--- src/miner.cpp
+++ src/miner.cpp
@@ -191,11 +191,6 @@ CBlockTemplate* BlockAssembler::CreateNewBlock(const CScript& scriptPubKeyIn)
     pblock->nNonce         = 0;
     pblocktemplate->vTxSigOpsCost[0] = WITNESS_SCALE_FACTOR * GetLegacySigOpCount(pblock->vtx[0]);
 
-    CValidationState state;
-    if (!TestBlockValidity(state, chainparams, *pblock, pindexPrev, false, false)) {
-        throw std::runtime_error(strprintf("%s: TestBlockValidity failed: %s", __func__, FormatStateMessage(state)));
-    }
-
     return pblocktemplate.release();
 }
 
